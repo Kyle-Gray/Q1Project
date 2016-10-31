17c0932af4577b0ac5083513d3304dda8357f221
Kyle Gray <th33nd3r@gmail.com>
2016-10-31 12:28:58 -0600

    fixed nav bar partially :construction:

--- js/DjKg.js
+++ js/DjKg.js
@@ -4,15 +4,15 @@
   // Api Call
   $(function(){
 $.getJSON("http://spotify.com/?s=", function(data){
-  var items = [];
-    $.each( data, function(key, val ) {
-      items.push( "<li id='" + key + "'>" + val + "</li>" );
-    });
-
-    $( "<ul/>", {
-      "class": "my-new-list",
-      html: items.join( "" )
-    }).appendTo( "body" );
+  // var items = [];
+  //   $.each( data, function( key, val ) {
+  //     items.push( "<li id='" + key + "'>" + val + "</li>" );
+  //   });
+  //
+  //   $( "<ul/>", {
+  //     "class": "my-new-list",
+  //     html: items.join( "" )
+  //   }).appendTo( "body" );
 });
 
 //knob functions
