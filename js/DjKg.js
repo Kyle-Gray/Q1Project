e28be7a3b62fb00c2985181e2b10c7c5daf3f4a4
Kyle Gray <th33nd3r@gmail.com>
2016-10-31 11:30:05 -0600

    added forms for where the apotify api information will go

--- js/DjKg.js
+++ js/DjKg.js
@@ -3,13 +3,31 @@
 (function($){
   // Api Call
   $(function(){
-$.getJSON("http://spotify.com/?s=");
+$.getJSON("http://spotify.com/?s=", function(data){
+  var items = [];
+    $.each( data, function(key, val ) {
+      items.push( "<li id='" + key + "'>" + val + "</li>" );
+    });
+
+    $( "<ul/>", {
+      "class": "my-new-list",
+      html: items.join( "" )
+    }).appendTo( "body" );
+});
 
 //knob functions
     $(".dial").knob({
-      'width':100%,
-      'height':50px
+      'min':-50,
+      'max':50,
+
     });
+    //add class to all dials
+    $("canvas").addClass("volumeDial");
+    //makes the dials the size we need
+$(".volumeDial").css("height", "25px");
+$(".volumeDial").css("width", "25px");
+// $(".volumeDial").css("float-left");
+
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
