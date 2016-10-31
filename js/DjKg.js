b1b72b7f4f646075f1b17de2e55acfb9612ebf52
Kyle Gray <th33nd3r@gmail.com>
2016-10-31 14:21:25 -0600

    added search for album by artist

--- js/DjKg.js
+++ js/DjKg.js
@@ -3,12 +3,12 @@
 (function($){
   // Api Call
   $(function(){
-$.getJSON("http://spotify.com/?s=", function(data){
-  var items = [];
-    $.each( data, function( key, val ) {
-      $("<tr>").append($("<td>"), items);
-    });
-});
+// $.getJSON("http://spotify.com/?s=", function(data){
+//   var items = [];
+//     $.each( data, function( key, val ) {
+//       $("<tr>").append($("<td>"), items);
+//     });
+// });
 
 //knob functions
     $(".dial").knob({
@@ -23,6 +23,28 @@ $(".volumeDial").css("height", "25px");
 $(".volumeDial").css("width", "25px");
 // $(".volumeDial").css("float-left");
 
+//spotifys get album art
+var resultsPlaceholder = document.getElementById('results');
+
+var searchAlbums = function (query) {
+  $.ajax({
+    url: 'https://api.spotify.com/v1/search',
+    data: {
+      q: query,
+      type: 'album'
+    },
+    success: function (response) {
+      console.log(response);
+      resultsPlaceholder.innerHTML = response;
+    }
+  });
+};
+document.getElementById('search-form').addEventListener('submit', function (e) {
+  e.preventDefault();
+  searchAlbums(document.getElementById('query').value);
+}, false);
 
   }); // end of document ready
+
+
 })(jQuery); // end of jQuery name space
