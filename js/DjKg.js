64aa8581afb324d43e73b428d75ecc39c7a8b545
Kyle Gray <th33nd3r@gmail.com>
2016-11-01 16:06:56 -0600

    added album ID to table and connected the corresponding information, all created second call to spotify to find the tracks of selected albums

--- js/DjKg.js
+++ js/DjKg.js
@@ -16,6 +16,19 @@
         var resultsPlaceholder = document.getElementById('results');
 
 
+        var getTracks = function(albumID){
+          $.ajax({
+            url: 'https://api.spotify.com/v1/albums/'+ albumID +'/tracks',
+            data: {
+              q: albumID,
+              type: 'track'
+            },
+            success: function (callback){
+              console.log(callback);
+            }
+          });
+        };
+
         var searchAlbums = function(query) {
             $.ajax({
                 url: 'https://api.spotify.com/v1/search',
@@ -27,25 +40,32 @@
                   // to get albums out of the object
                     var arr1 = [];
                     var arr2 = [];
+                    var arr3 = [];
                     var albumArray = response.albums.items;
                     for (var i = 0; i < albumArray.length; i++) {
                       var albumArtArray = albumArray[i]["images"];
-
                       arr1.push(albumArray[i]["name"]);
                       arr2.push(albumArtArray[2]["url"]);
+                      arr3.push(albumArray[i]["id"]);
                       // console.log(arr1);
 
                       var newRow = $("<tr>");
                       var newColumn = $("<td>");
                       var albumNames = $("<td>");
+                      var albumID = $("<td>");
                       $("#maintable").append(newRow);
-                      newRow.append(newColumn, albumNames);
-                       newColumn.append("<img src=" + arr2[i] + ">");
+                      newRow.append(newColumn, albumNames, albumID);
+                       newColumn.append("<img class= 'albumArts' src=" + arr2[i] + ">");
                        albumNames.append(arr1[i]);
-
+                       albumID.append(arr3[i]);
                     }
+                    $(".albumArts").on('click', function(){
+                      // console.log($(this).text());
+                      $("#search").toggle();
+                      $("#hiddensearch").toggle();
+                    });
 
-                    // console.log(response);
+                    console.log(response);
                     // resultsPlaceholder.innerHTML = response;
                 }
             });
@@ -56,6 +76,17 @@ document.getElementById('search-form').addEventListener('submit', function(e) {
     e.preventDefault();
     searchAlbums(document.getElementById('query').value);
   }, false);
+
+document.getElementById('hiddensearch').addEventListener('submit', function(e) {
+      e.preventDefault();
+      getTracks(document.getElementById('albumID').value);
+    }, false);
+// click on Album Artwork to push picture up by turntables as well as being able to search tracks
+
+
+
+
+
 
 // Get tracks from within the albums that we just searched
 
